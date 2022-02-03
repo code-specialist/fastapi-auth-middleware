5649edf81a9efa038cd4a69af2776c74bd0e4527
Yannic Schroeer <yannic.schroeer@sap.com>
2022-02-03 12:20:17 +0100

    docs cleaned

--- fastapi_auth_middleware/__init__.py
+++ fastapi_auth_middleware/__init__.py
@@ -1,6 +1,6 @@
 """ Lightweight auth middleware for FastAPI that just works. Fits most auth workflows with only a few lines of code. """
 
-__version__ = "0.0.1"
+__version__ = "0.1a0"
 
 from fastapi_auth_middleware.middleware import FastAPIUser, AuthMiddleware
 
