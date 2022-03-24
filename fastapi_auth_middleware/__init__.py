91a2d40a1232170acd8cebfc13bb31f0d9c0fc3a
Yannic Schröer <yannicschroer@Yannics-MBP.fritz.box>
2022-03-24 17:45:11 +0100

    v1.0.1

--- fastapi_auth_middleware/__init__.py
+++ fastapi_auth_middleware/__init__.py
@@ -1,6 +1,6 @@
 """ Lightweight auth middleware for FastAPI that just works. Fits most auth workflows with only a few lines of code. """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from fastapi_auth_middleware.middleware import FastAPIUser, AuthMiddleware
 from fastapi_auth_middleware.oauth2_middleware import OAuth2Middleware
